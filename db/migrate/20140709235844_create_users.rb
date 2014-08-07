ffe3dc9bcefcff6097fd3c4262df69d92792d9d5
Al Roorda <aroorda85@gmail.com>
2014-08-06 18:05:38 -0700

    Fixed CreateUsers migrate

--- db/migrate/20140709235844_create_users.rb
+++ db/migrate/20140709235844_create_users.rb
@@ -1,14 +1,10 @@
-class CreateRelationships < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+    create_table :users do |t|
+      t.string :name
+      t.string :email
 
       t.timestamps
     end
-
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
